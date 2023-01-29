cec280c694b332aa560c3d9e7bc6f13e5b39ffe1
Jobson Lira <jobson.lira@gmail.com>
2023-01-29 16:17:56 -0300

    feat: adicioando thema claro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export const AppRoutes = () => {
 
     return(
         <Routes>
-            <Route path='/pagina-inicial' element={<Button>Página inicial</Button>}/>
+            <Route path='/pagina-inicial' element={<Button variant='contained' color='primary' >Página inicial</Button>}/>
             
             <Route path='*' element={<Navigate to ="/pagina-inicial"/>}/>
         </Routes>
