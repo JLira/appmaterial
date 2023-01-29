86f3edbe7ad14eabc4ef0d6799b80b3abf43c97e
Jobson Lira <jobson.lira@gmail.com>
2023-01-29 12:09:40 -0300

    feat: Instalin g Material UI

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Cadastros
+Instalar a biblioteca material em mui.com/pt/
+>npm install @mui/icons-material
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
+import { Button } from '@mui/material';
+import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const AppRoutes = () => {
 
     return(
         <Routes>
-            <Route path='/pagina-inicial' element={<p>Página inicial</p>}/>
+            <Route path='/pagina-inicial' element={<Button>Página inicial</Button>}/>
             
             <Route path='*' element={<Navigate to ="/pagina-inicial"/>}/>
         </Routes>
