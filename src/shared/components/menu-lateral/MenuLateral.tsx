5d5fc5cad4809f3c07dd4742dd55bd57121f87c5
Jobson Lira <jobson.lira@gmail.com>
2023-01-30 11:18:44 -0300

    feat: adicionado responsividade  no menu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,5 +1,9 @@
-import { Avatar, Drawer, useTheme, Divider, List, ListItemButton,ListItemIcon,ListItemText, Icon } from '@mui/material';
+import {
+  Avatar, Drawer, useTheme, Divider, List, ListItemButton,
+  useMediaQuery, ListItemIcon, ListItemText, Icon
+} from '@mui/material';
 import { Box } from '@mui/system';
+import { useDrawerContext } from '../../contexts';
 
 
 
@@ -10,10 +14,13 @@ interface IAppMenuLateralProps {
 
 export const MenuLateral: React.FC<IAppMenuLateralProps> = ({ children }) => {
   const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
 
   return (
     <>
-      <Drawer variant='permanent'>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
         <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
           <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
             <Avatar
@@ -26,9 +33,9 @@ export const MenuLateral: React.FC<IAppMenuLateralProps> = ({ children }) => {
             <List component="nav">
               <ListItemButton>
                 <ListItemIcon>
-                  <Icon>home</Icon>   
+                  <Icon>home</Icon>
                 </ListItemIcon>
-                <ListItemText primary="Página inicial"/>
+                <ListItemText primary="Página inicial" />
               </ListItemButton>
             </List>
           </Box>
@@ -36,7 +43,7 @@ export const MenuLateral: React.FC<IAppMenuLateralProps> = ({ children }) => {
         </Box>
       </Drawer>
 
-      <Box height="100vh" marginLeft={theme.spacing(28)}>
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       </Box>
     </>
